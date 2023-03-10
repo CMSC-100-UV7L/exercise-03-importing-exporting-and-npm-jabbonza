f74b3dc6704a412ecff81b0787745d7bd9109ec4
jabbonza <jbbonza1@up.edu.ph>
2023-03-10 13:57:25 +0800

    test: imported addAccount and added 2 test cases

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-const { generateUniqueID } = require('./function');
+const { generateUniqueID, addAccount } = require('./function');
 
 console.log(generateUniqueID("Alan", "Turing"));
 console.log(generateUniqueID("Tim", "Berners-Lee"));
 
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
+console.log(addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
